00ae5f9777fd0e5bf88afa1ebaac4be70500ec4d
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-24 13:10:47 -0700

    Update IO.c

--- IO.c
+++ IO.c
@@ -1,11 +1,17 @@
 #include "stdio.h"
 int main ()
 {
-    char [] prompt = "Enter a number, a character, and a string of characters: ";
+    char prompt []  = "Enter a number, a character, and a string of characters: ";
     char letter;
-    char [] char_string;
+    char  *char_string  [25];
     int value;
   	printf("Hello World!\n");
   	scanf("%d %c %s", &value, &letter, &char_string);
+  	int i =0;
+  	while (char_string[i] != "/0")
+  	{
+  	    printf("%s", char_string[i]);
+  	    i++;
+  	}
   return 0;
 }
