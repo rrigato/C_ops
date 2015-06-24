f71b22883e384409b032eb33e92d39edb2f57635
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-24 13:00:16 -0700

    Create IO.c

---
+++ IO.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+int main ()
+{
+    char [] prompt = "Enter a number, a character, and a string of characters: ";
+    char letter;
+    char [] char_string;
+    int value;
+  	printf("Hello World!\n");
+  	scanf("%d %c %s", &value, &letter, &char_string);
+  return 0;
+}
