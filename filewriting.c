34a21d1c1f6c831eadeaf9609511bc12890329c8
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-25 15:17:47 -0700

    Update filewriting.c

--- filewriting.c
+++ filewriting.c
@@ -3,5 +3,11 @@
 #include "string.h"
 int main (int argc, char * argv [])
 {
+    FILE * fp =NULL;
+    //open for writing
+    fp = fopen("output.txt", "w");
+    if (!fp)
+        exit (1);
+    fclose(fp);
     return 0;
 }
