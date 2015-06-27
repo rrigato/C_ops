4baab2549da2d7653a4536ed3c76c6160f20bac8
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-27 16:27:48 -0700

    Update replicate_cat.c

--- replicate_cat.c
+++ replicate_cat.c
@@ -1,27 +1,39 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 void arguement_change ( char *, char *, char *, int, char * []);
 int cat_check ( char *);
 int main ( int argc, char * argv [])
 {
 	//exits the program if three arguements are not provided
-	if (argc != 3)
-		exit(1);
+	//if (argc != 4)
+	//	exit(1);
 	// gets seperate pointers for each string to test which commands the user entered.
 	char * first_arg = argv[1];
 	char * second_arg = argv[2];
 	char * third_arg = argv[3];
-	printf("%s \n", first_arg);
-	if (! cat_check (first_arg))
+
+	//check to make sure command is cat
+	if (!cat_check (first_arg))
 		exit(1);
-	printf("%s \n", first_arg);
+	FILE * fp = NULL;
+	fp = fopen("MyQueue.cpp", r+);
+	if (fp)
+	{
+		fscanf(
+		fclose (fp);	
+
+	}
+
+
 	
 	
 }
 int cat_check (char * first_arg)
 {
-	if (first_arg =="cat")
+	char CAT [] = "cat";
+	if (!strcmp(first_arg, CAT))
 		return 1;
 	else return 0;
 }
