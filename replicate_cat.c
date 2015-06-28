4e195685b199d863b410906b783139a3c3425fcc
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-27 20:30:45 -0700

    Update replicate_cat.c

--- replicate_cat.c
+++ replicate_cat.c
@@ -3,6 +3,7 @@
 #include "string.h"
 //can we assume that you will use a switch
 //and the order of arguements will be ./ <executable name> cat -nET "file.txt"
+//formating of -n switch
 void arguement_change ( char *, char *, char *, int, char * []);
 int cat_check ( char *);
 int main ( int argc, char * argv [])
@@ -29,8 +30,8 @@ int main ( int argc, char * argv [])
 	int count = 0;
 	if (fp)
 	{
-		
-		printf("     %d ", i);
+		if (second_arg[1]== 'n' || second_arg[2] == 'n' || second_arg[3] == 'n')
+			printf("\t%d ", i);
 		while ((letter = getc(fp))!=EOF)
 		{
 			//checks if any of elements 1-3 of second arguement have the switch of T
@@ -38,8 +39,8 @@ int main ( int argc, char * argv [])
 			if ( letter == '\t' && (second_arg[1] =='T' || second_arg[2] == 'T' ||second_arg[3] =='T'))
 				printf ("^I");
 			else printf("%c", letter);
-			{if (letter == '\n')
-				{++i; printf("     %d ", i);}
+			{if (letter == '\n' && (second_arg[1]== 'n' || second_arg[2] == 'n' || second_arg[3] == 'n'))
+				{++i; printf("\t%d ", i);}
 			}
 	/*i++;
 		
