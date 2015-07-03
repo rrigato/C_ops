68e9a31ea3b77c46627aab83e1f3a0c20d3a89a1
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-02 21:11:18 -0700

    Update IO.c

--- IO.c
+++ IO.c
@@ -4,7 +4,6 @@
 //can we assume that you will use a switch
 //and the order of arguements will be ./ <executable name> cat -nET "file.txt"
 //formating of -n switch leaves a 
-void arguement_change ( char *, char *, char *, int, char * []);
 int cat_check ( char *);
 int main ( int argc, char * argv [])
 {
@@ -24,32 +23,52 @@ int main ( int argc, char * argv [])
 
 	int Switch_count ;
 	FILE * fp = NULL;
-	fp = fopen(third_arg, "r");
+	if (argc ==3)
+	{
+		fp = fopen(third_arg, "r");
+	}
+	else if (argc ==2)
+	{
+		fp = fopen(second_arg, "r");
+	}
 	char letter ;
 	int i = 1;
 	int count = 0;
 	if (fp)
 	{
-		if (second_arg[1]== 'n' || second_arg[2] == 'n' || second_arg[3] == 'n')
-			printf("\t%d ", i);
-		while ((letter = getc(fp))!=EOF)
+		if (argc ==3)
 		{
-			//checks if any of elements 1-3 of second arguement have the switch of T
-			//element 0 will not have a switch because that has the dash for the option
-			if ( letter == '\t' && (second_arg[1] =='T' || second_arg[2] == 'T' ||second_arg[3] =='T'))
-				printf ("^I");
-			else 
 
+			if (second_arg[1]== 'n' || second_arg[2] == 'n' || second_arg[3] == 'n')
+				printf("\t%d ", i);
+			while ((letter = getc(fp))!=EOF)
 			{
-				//checks letter read in for escape clause and prints a $ if E is selected as an option BEFORE the 
-				//escape clause is shown on the screen
-				if(letter == '\n' && (second_arg[1]== 'E' || second_arg[2] == 'E' || second_arg[3] == 'E'))
-						{ printf("$");}
-				printf("%c", letter);
-				//checks letter read in for escape clause and prints line number AFTER the line is returned
-				if (letter == '\n' && (second_arg[1]== 'n' || second_arg[2] == 'n' || second_arg[3] == 'n'))
-					{++i; printf("\t%d ", i);}
+				//checks if any of elements 1-3 of second arguement have the switch of T
+				//element 0 will not have a switch because that has the dash for the option
+				if ( letter == '\t' && (second_arg[1] =='T' || second_arg[2] == 'T' ||second_arg[3] =='T'))
+					printf ("^I");
+				else 
+
+				{
+					//checks letter read in for escape clause and prints a $ if E is selected as an option BEFORE the 
+					//escape clause is shown on the screen
+					if(letter == '\n' && (second_arg[1]== 'E' || second_arg[2] == 'E' || second_arg[3] == 'E'))
+							{ printf("$");}
+					printf("%c", letter);
+					//checks letter read in for escape clause and prints line number AFTER the line is returned
+					if (letter == '\n' && (second_arg[1]== 'n' || second_arg[2] == 'n' || second_arg[3] == 'n'))
+						{++i; printf("\t%d ", i);}
+				}
 			}
+		}
+
+		else if (argc ==2) 
+		{
+			while ((letter = getc(fp))!= EOF)
+			{
+				printf("%c", letter);
+			}
+		}
 
 		fclose (fp);	
 
@@ -71,7 +90,4 @@ int cat_check (char * first_arg)
 	else return 0;
 }
 
-void arguement_change ( char * first, char * second, char * third, int argc, char * argv[])
-{
-	
-}
+
