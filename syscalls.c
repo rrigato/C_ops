8ad46e60efa3b751e2711f0a0273cfe5d4bf1fd9
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-10 12:10:35 -0700

    Update syscalls.c

--- syscalls.c
+++ syscalls.c
@@ -76,16 +76,50 @@ void with_switches(char * file_holder, char * switches)
 	int tab = 0;
 	int line_num =0;
 	int doller_sign =0;
+	int line_and_doller =0;
+	int doller_and_tab = 0;
+	int tab_and_line = 0;
+	int all_three =0;
 	if (switches[1]=='T'|| switches[2]=='T'||switches[3]=='T')
+	{	
 		tab =1;
+	}
 	if (switches[1]=='E'|| switches[2]=='E'||switches[3]=='E')
+	{	
 		doller_sign = 1;
+	}	
 	if (switches[1]=='n'|| switches[2]=='n'||switches[3]=='n')
+	{
 		line_num = 1;
+	}
+	if ((switches[1]=='n'|| switches[2]=='n'||switches[3]=='n') && (switches[1]=='E'|| switches[2]=='E'||switches[3]=='E'))
+	{
+		line_and_doller = 1;	
+	}
+	if ((switches[1]=='T'|| switches[2]=='T'||switches[3]=='T') && (switches[1]=='E'|| switches[2]=='E'||switches[3]=='E'))
+	{
+		doller_and_tab = 1;	
+	}
+	if ((switches[1]=='T'|| switches[2]=='T'||switches[3]=='T') && (switches[1]=='n'|| switches[2]=='n'||switches[3]=='n'))
+	{
+		tab_and_line = 1;	
+	}
+	if ((switches[1]=='n'|| switches[2]=='n'||switches[3]=='n') && (switches[1]=='E'|| switches[2]=='E'||switches[3]=='E') 
+		&& (switches[1]=='T'|| switches[2]=='T'||switches[3]=='T'))
+	{
+		all_three =1;	
+	}
+
 	int i = 0;
+	int line_counter =1;
+	if (line_num)
+	{
+		printf("     1  ");
+		line_counter++;
+	}
 	while (file_holder[i] != '\0')
 	{
-		if (tab && (file_holder[i] =='\t'))
+		/*if (tab && (file_holder[i] =='\t'))
 		{			
 			printf("^I");
 		}
@@ -93,13 +127,86 @@ void with_switches(char * file_holder, char * switches)
 		{
 			printf("$\n");
 		
+		}*/
+
+		if(line_and_doller==1)
+		{
+			if ((file_holder[i] !='\n'))
+			{
+				printf("%c", file_holder[i]);
+			}
+			else
+			{
+				if(file_holder[i+1] == '\0')
+				{
+					printf("$");
+					printf("%c",file_holder[i]);
+					//prevents extra number at end of output
+				}
+				else
+				{
+					printf("$");
+					printf("%c", file_holder[i]);
+					printf("     %d  ", line_counter);
+					line_counter++;
+				}		
+			}
+			i++;
 		}
-		if (line_num && (file_holder[i] =='\n'))
-		else
-		{	
-			printf("%c", file_holder[i]);
+		if (all_three)
+		{
+			if ((file_holder[i] !='\n'))
+			{
+				if (file_holder[i] =='\t')
+				{			
+					printf("^I");
+				}
+				else
+				{
+					printf("%c", file_holder[i]);
+				}
+			}
+			else
+			{
+				if(file_holder[i+1] == '\0')
+				{
+					printf("$");
+					printf("%c",file_holder[i]);
+					//prevents extra number at end of output
+				}
+				else
+				{
+					printf("$");
+					printf("%c", file_holder[i]);
+					printf("     %d  ", line_counter);
+					line_counter++;
+				}		
+			}
+			i++;
+			
 		}
-		i++;
+		if (doller_and_tab)
+		{
+			if (file_holder[i] =='\t')
+				{ printf("^I"); }
+			else if (file_holder[i]=='\n')
+				{ printf("$"); printf("%c",file_holder[i]); }
+			else
+				{ printf("%c",file_holder[i]); }
+			i++;
+		}
+		
+		if (doller_and_tab)
+		{
+			if (file_holder[i] =='\t')
+				{ printf("^I"); }
+			else if (file_holder[i]=='\n')
+				{ printf("$"); printf("%c",file_holder[i]); }
+			else
+				{ printf("%c",file_holder[i]); }
+			i++;
+		}
 	}
 
 }
+
