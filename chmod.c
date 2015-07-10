96f7e7d45732066892dc63c03f2642745a85a10f
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-10 16:52:34 -0700

    Create chmod.c
    Uses system calls to the unix kernal to write the chmod system command

---
+++ chmod.c
@@ -0,0 +1,36 @@
+//I just passed it a decimal number is that correct?
+//converted the char to integer using atoi()
+#include "fcntl.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+void error_handle (char * words, char * error_arg);
+int main ( int argc, char * argv [ ])
+{
+	if (argc !=3)
+	{
+		printf ("chmod: missing operand \n");
+		printf("Try 'chmod --help' for more information");
+		exit (1);
+	}
+	char * permission_change = argv[1];
+	int value = atoi(permission_change);
+	char * file_name = argv[2];
+	int outcome = 0;
+	outcome = chmod(file_name, value);
+	if (outcome == -1 )
+		error_handle("Error reading argument: ", file_name);
+	else 
+		printf("It surprisingly worked.");
+	
+	
+}
+
+void error_handle (char * words, char * error_arg)
+{
+	printf( "%s\n", words);
+	perror(error_arg); 
+	exit(1);
+}
