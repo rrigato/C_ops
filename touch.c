5a999a2fc0e9b70e22bb8425011a38e3826db36f
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-11 11:33:38 -0700

    Update touch.c

--- touch.c
+++ touch.c
@@ -1,1 +1,29 @@
 
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+//int touch_check ( char *);
+int main ( int argc, char * argv [])
+{
+	/*char * arg_0 = argv[0];
+	if (touch_check(arg_0))
+	{
+		//good to proceed with the program
+		printf("good to go");
+	}
+	else
+	{
+		printf("Error improper system command\n");
+		exit(1);
+	}*/
+	return 0;
+}
+/*
+int touch_check (char * arg_0)
+{
+	char compare [] = "./touch";
+	if (!strcmp(arg_0, compare) ==0)
+		return 1;
+	return 0;
+}*/
