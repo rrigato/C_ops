7794823582e75eff211a6ca93fa217e454962120
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-12 10:44:12 -0700

    Update touch.c

--- touch.c
+++ touch.c
@@ -1,9 +1,17 @@
+#include "fcntl.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "sys/time.h"
+#include "sys/types.h"
+#include "utime.h"
+#include "time.h"
 
 //int touch_check ( char *);
 int touch_options( char *);
+void change_both (char *);
+void change_access(char*);
+void change_mod(char*);
 int main ( int argc, char * argv [])
 {
 	/*char * arg_0 = argv[0];
@@ -20,6 +28,7 @@ int main ( int argc, char * argv [])
 	char * option_check = NULL;
 	char * file_name = NULL;
 	option_check = argv[1];
+	int options = 0;
 	if (argc ==1)
 	{
 		printf("touch: missing file operand\n");
@@ -28,15 +37,82 @@ int main ( int argc, char * argv [])
 	}
 	if (option_check[0] =='-')
 	{
-		int options = 0;
 		options = touch_options(option_check);
+		if (options ==1)
+		{
+			file_name = argv[3];
+		}
+		else
+		{
+			file_name = argv[2];
+		}
 	}
 	else
 	{
 		option_check = NULL;
 		file_name = argv[1];
 	}
+	if (argc ==2)
+	{
+		int file_descriptor = 0;
+		//two inefficient syscalls to check and see if the file exists
+		if ((file_descriptor= open(file_name, O_RDONLY))==-1)
+		{
+			int Create = 0;
+			// sets the file to rw-rw-r-- by default
+			Create = creat(file_name, 0664);
+			if (Create == -1)
+			{
+				printf("Could not create a new file");
+				exit(1);
+			}
+			else 
+			{
+				//printf("file created\n");
+				//A new file was created
+			}
+		}
+		else 
+		{
+			change_both(file_name);	
+		}
+		close (file_descriptor);	
+	}
+	else if (argc ==3)
+	{
+		if (options == 3)
+		{
+			change_both(file_name);
+		}
+	}
 	return 0;
+}
+void change_both (char * path)
+{
+
+
+	int time = 0;	
+	time = utime(path, NULL);
+	if (time == -1 )
+	{
+		printf("Error: unable to modifiy access and modification time.\n");
+		exit(1);
+	}
+	else 
+	{
+		//function changed both the access and modification time to current time.
+	}
+}
+void change_access(char * second_arg)
+{
+	struct utimbuf current_time;
+	current_time.actime ;
+	current_time.modtime;
+	
+}
+void change_mod(char * second_arg)
+{
+	
 }
 
 int touch_options(char * second_arg)
