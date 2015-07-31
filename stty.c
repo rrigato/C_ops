0f2296862f875f0c747098f49331055aed64e4bf
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-31 10:05:18 -0700

    Update stty.c

--- stty.c
+++ stty.c
@@ -2,6 +2,10 @@
 #include "stdlib.h"
 #include "string.h"
 int option(char *);
+void echo_option( int, char *);
+void a_option (char *);
+void erase(char *, char *);
+void iuclc_option(int, char *);
 int main(int argc, char * argv[])
 {
 	if (argc !=2 && argc!= 3)
@@ -12,9 +16,34 @@ int main(int argc, char * argv[])
 	}
 	
 	char * option_check = argv[1];
-	option(option_check);
+	int return_value = 0;
+	
+	return_value = option(option_check);
+	switch(return_value){
+		case 1:
+			echo_option(return_value, option_check);
+			break;
+		case 2:
+			echo_option(return_value, option_check);
+			break;
+		case 3:
+			a_option(option_check);
+			break;
+		case 4:
+			{
+				char * new_erase_key = argv[2];
+				erase(option_check, new_erase_key);
+				break;
+			}
+		default:
+			iuclc_option(return_value, option_check);
+			break;
+	
+	}
 	return 0;
 }
+
+
 
 
 //  returns 1 if echo, 2 if -echo, 3 if -a, 4 if erase, 5 if iuclc, 6 if -iuclc
@@ -68,4 +97,24 @@ int option(char * option_check)
 		exit(1);
 	}
 	return selected;
+}
+
+
+void echo_option( int value, char * option_name)
+{
+	printf("%d\n", value);
+}
+
+void a_option (char * option_name)
+{
+	printf("3\n");
+}
+void erase(char * option_name, char * new_key)
+{
+	printf("4\n");
+}
+
+void iuclc_option(int value, char * option_name)
+{
+	printf("%d\n",value);
 }
