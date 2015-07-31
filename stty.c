b7cffc15760063a104c4d024646bdd9ad138a0c8
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-31 09:51:28 -0700

    Update stty.c

--- stty.c
+++ stty.c
@@ -16,44 +16,52 @@ int main(int argc, char * argv[])
 	return 0;
 }
 
-//adds 10 to the return value if it is has a dash
-// otherwise returns 1 if echo, 2 if a, 3 if erase, 4 if iuclc 
+
+//  returns 1 if echo, 2 if -echo, 3 if -a, 4 if erase, 5 if iuclc, 6 if -iuclc
+//prints an error otherwise 
 int option(char * option_check)
 {
 
 	int selected = 0;	
 	char echo[] = "echo";
+	char dash_echo[] = "-echo";
 	char a[] = "-a";
 	char erase[] = "erase";
 	char iuclc[] = "iuclc";
-	char dash ='-';
-	if (option_check[0] == dash)
-	{
-		selected += 10;
-		printf("%d\n", selected);
-	}
-	if (strstr(option_check, echo) != NULL)
+	char dash_iuclc[] = "-iuclc";
+
+	if (strcmp(option_check, echo) == 0)
 	{
 		selected += 1;
 		printf("%d\n", selected);
 	}
-	if (strstr(option_check, a) != NULL)
+	if (strcmp(option_check, dash_echo) == 0)
 	{
 		selected += 2;
 		printf("%d\n", selected);
 	}
-	if (strstr(option_check, erase) != NULL)
+	if (strcmp(option_check, a) == 0)
 	{
 		selected += 3;
 		printf("%d\n", selected);
 	}
-	
-	if (strstr(option_check, iuclc) != NULL)
+	if (strcmp(option_check, erase) == 0)
 	{
 		selected += 4;
 		printf("%d\n", selected);
 	}
-	if (selected == 0 || selected ==10)
+	
+	if (strcmp(option_check, iuclc) == 0)
+	{
+		selected += 5;
+		printf("%d\n", selected);
+	}
+	if (strcmp(option_check, dash_iuclc) == 0)
+	{
+		selected += 6;
+		printf("%d\n", selected);
+	}
+	if (selected == 0 )
 	{
 		printf("stty: invalid arguement %s\n", option_check);
 		printf("Try 'stty--help' for more information.\n");
@@ -61,4 +69,3 @@ int option(char * option_check)
 	}
 	return selected;
 }
-
