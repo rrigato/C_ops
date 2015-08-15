fb20177c79242238dcfb86c396cb0a8f1834f52d
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-08-14 18:59:22 -0700

    Update signal.c

--- signal.c
+++ signal.c
@@ -1,11 +1,20 @@
-
 #include "stdio.h"
-
+#include "signal.h"
+#include "stdlib.h"
+#include "math.h"
 #define n 100000000
 void sieve();
+void display(int);
+void display2(int);
 int main (int argc, char * argv[])
 {
 	printf("%d\n", n);
+	int Continue = 1;
+	signal(SIGINT,SIG_IGN);
+	signal(SIGINT, display);
+	signal(SIGQUIT, display2);
+	sieve();
+	
 	return 0;
 }
 
@@ -14,11 +23,11 @@ void sieve ()
 	int table[n];
 	int z = 0;
 	for (; z< n; z++)
-		table[n] = 1;
+		table[z] = 1;
 	int i=2;
 	int j = 0;
 	int k =0;
-	int n_square = (*int)sqrt((*double)n);
+	int n_square = sqrt(n);
 	for (; i < n_square; i++)
 	{
 		if (table[i])
@@ -27,9 +36,26 @@ void sieve ()
 			do
 			{
 				table[j] = 0;
+				
 				k++;
+				
 				j = (i*i)+(i*k);
 			}while(j<n);
 		}
+	}
+	
+}
+
+void display(int signum)
+{
+	if (signum ==2)
+	printf("It worked\n");
+}
+void display2(int signum)
+{
+	if (signum==3)
+	{
+	printf("It worked\n");
+	exit(1);
 	}
 }
