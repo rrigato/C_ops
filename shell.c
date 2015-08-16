b5f9a25bc842fd08d6d5070e91b7baf68037ffbf
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-08-16 15:44:59 -0700

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,12 +1,41 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "signal.h"
+#include "string.h"
 #define size 256
 int main ()
 {
 	char input[size];
 	signal(SIGINT, SIG_IGN);
-	fgets(input,size, stdin);
-	printf("%s\n",input);
+
+	char  Stop[] = "quit\n" ;
+
+	
+	while(fgets(input, size, stdin) && strcmp(input, Stop))
+	{	
+		
+	}
 	return 0;
+}
+
+
+char** parse( char* str )
+{
+    char *token;
+    char tokens[256];
+    char** args = malloc( 256 );    
+    int i = 0;
+       
+    strcpy( tokens, str );
+    
+    args[i] = strtok( tokens, " " );
+    
+    while( args[i] )
+    {
+    
+        i++;
+        args[i] = strtok(NULL, " ");
+    } 
+ 
+    return args;
 }
