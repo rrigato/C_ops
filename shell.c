5f823395700e2eaf422fab342d7571ae9e16af40
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-08-18 18:06:58 -0700

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,56 +2,56 @@
 #include "stdlib.h"
 #include "signal.h"
 #include "string.h"
-#define size 256
+#include "unistd.h"
+#define SIZE 256
 
-char** parse( char*  );
-void run (char * []);
+char** parse( char* );
+void run (char **);
 int main ()
 {
-	char input[size];
+	char input[SIZE];
 	signal(SIGINT, SIG_IGN);
 
 	char  Stop[] = "quit\n" ;
 	
 	char** Command  = NULL; 
-	int i,z;
 	printf( " > "); 
-	while(fgets(input, size, stdin) && strcmp(input, Stop))
-	{	
-		
+	while(fgets(input, SIZE-1, stdin) && strcmp(input, Stop))
+	{
+		int i = 0;
+		for (; i < SIZE-2;i++) //replaces newline character
+			if (input[i] = '\n')
+				input[i] = '\0';
+
 		Command = parse(input);
 		run(Command);
-		 // i =0;
-		 // z= 0;
- /*		for (; Command[i] != '\0'; i++)
-			printf("%s\n" ,Command[i]); */  //should print out array of c strings
-//		 z =0
-//		for (; Command[z][0] != '\0'; z++)
-//			free(Command[z])
 		free(Command);
 		printf( " > "); 
+		Command = NULL;
 		
 		
 	}
 	return 0;
 }
 
-void run(char ** Arglist)
+void run(char ** Arglist )
 {
 	int fork_return;
 	int wait_value;
 	fork_return = fork();
 	if (fork_return == -1)
-		{
-			perror("You forked up!");
-			exit(1);
-		}
+	{
+		perror("You forked up!");
+		exit(1);
+	}
 	else if (fork_return ==0)
-		{
-			execvp(Arglist[0], Arglist);
-			perror("execvp failed");
-			exit(1);
-		}
+	{	
+
+		execvp(Arglist[0], Arglist);
+		perror("execvp failed");
+		exit(1);
+		
+	}
 	else 
 	{
 		wait(&wait_value);
