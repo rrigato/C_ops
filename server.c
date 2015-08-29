d7ae674655110238d1ba1b772d8f107782148442
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-08-28 17:40:22 -0700

    Update server.c

--- server.c
+++ server.c
@@ -1,47 +1,60 @@
-
 #include "fcntl.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 #include "sys/stat.h"
 #include "sys/types.h"
 #include "unistd.h"
 #include "time.h"
 int main ()
 {
-	char time_fifo[] = "tmp_pipe2";
+		char time_fifo[] = "tmp_pipe2";
 
- 	if (mkfifo(time_fifo, 0666) == -1)
-		perror("Fifo:");
-	
-	
-	int file_descriptor = open(time_fifo, O_WRONLY);
-	if (file_descriptor  == -1 )
-		{
-			perror("Unable to open named pipe");
-			exit(1);
+		if (mkfifo(time_fifo, 0666) == -1)
+		{	
+			perror("Fifo:");
 		}
+		time_t get_time;
+		char *time_string = NULL;
+
+		//printf("%s\n", time_string);  this prints
 	
-	time_t get_time;
-	char *time_string = NULL;
- 
-	close(file_descriptor);			
-				time(&get_time);
-				time_string = ctime(&get_time);
-				printf("%s\n", time_string);
-				if (write(file_descriptor, time_string, sizeof(time_string) ) == -1 )
+	
+		int file_descriptor;
+		int i = 0, counter = 0;
+		while(1)
+		{
+					time(&get_time);
+					time_string = ctime(&get_time);
+					//printf("%s\n", time_string);
+				 file_descriptor  = open(time_fifo, O_WRONLY);
+				if (file_descriptor  == -1 )
+					{
+						perror("Unable to open named pipe");
+						exit(1);
+					}
+/* 				counter  = sizeof(time_string);
+				 i = 0;
+				while(i < counter)
+				{
+					write(file_descriptor, (time_string + i* sizeof(char)) , sizeof(char));
+					i++;
+				} */
+				 if (write(file_descriptor, time_string, strlen(time_string)+1) == -1 )
 					{
 						perror("Write error:");
 						exit(1);
 				
-					}
+					} 
+				sleep(1);
+				if (close(file_descriptor) == -1)
+				{
+					perror("closing named pipe");
+					exit(1);
+				}
 
+		}
 		
-		
-	if (close(file_descriptor) == -1)
-	{
-		perror("closing named pipe");
-		exit(1);
-	}
 
 	unlink(time_fifo);
 	return 0;
