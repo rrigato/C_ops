d3708ec60a48e9baeb60eebe8526edb25740018e
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-09-02 14:09:16 -0700

    Update pipe_practice.c

--- pipe_practice.c
+++ pipe_practice.c
@@ -1,9 +1,23 @@
+int main(int argc, char * argv[])
 {
 	int thepipe[2];
 	pipe(thepipe);
 	int return_value;
 	return_value = fork();
 	if (return_value ==0)
+	{		
+		close(thepipe[0]);
+		dup2(thepipe[1], 1);
+		close(thepipe[1]);
+		excelp(argv[1], argv[1], NULL);
+
+
+		
+	}
+	else
 	{
-		excelp(argv[1], argv[2]
+		close(thepipe[1]);
+		dup2(thepipe[0],0);
+		execlp(argv[0],0);
+	}
 }
